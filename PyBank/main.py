4e9d021383e92e77bb1bc88923fe29cdef73e07b
Ally Qi <mcally1220@hotmail.com>
2020-03-24 18:41:13 -0500

    Updated on 3.24.20

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,62 +9,65 @@ import csv
 # Relative Path
 csvpath = os.path.join(os.path.expanduser('~'),"Documents/Classwork/vu-nsh-data-pt-02-2020-u-c", "03-Python/Homework/Instructions/PyBank/Resources/budget_data.csv")
 
-# build new variables to store either lists or numbers
-new_budget_dict = []
-month_list = []
-avgchanges = []
-amount_list = []
-total_amount = 0
-changes = 0
-total_changes = 0
+def analyze_budget(path):
 
-#open the csv file
-with open(csvpath) as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        month = row["Date"]
-        month_list.append(row["Date"])
-        amount = row["Profit/Losses"]
-        total_amount += int(row["Profit/Losses"])
-        amount_list.append(int(row['Profit/Losses']))
-        new_budget_dict.append(
-            {
-                "Month": row["Date"],
-                "Amount": row["Profit/Losses"]
-            }
-        )
-     #find the monthly changes
-    for x in range(len(amount_list)):
-        changes = amount_list[x] - changes
-        avgchanges.append(changes)
-        total_changes += changes
-        changes = amount_list[x] #reset the changes variable to the previous amount
+    # build new variables to store either lists or numbers
+    new_budget_dict = []
+    month_list = []
+    avgchanges = []
+    amount_list = []
+    total_amount = 0
+    changes = 0
+    total_changes = 0
 
-#create a new list to store the date and monthly changes    
-changes_date_list = list(zip(month_list, avgchanges))
-#find total number of months
-total_months = len(new_budget_dict)
-#find the average change over time in profit
-avgchanges2 = round((total_changes-avgchanges[0])/(total_months-1), 2)
-#find the max increase in profit
-maxprofit= max(changes_date_list, key=lambda x:x[1])
-#find the max decrease in profit
-minprofit=min(changes_date_list, key=lambda x:x[1])
+    #open the csv file
+    with open(csvpath) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            month = row["Date"]
+            month_list.append(row["Date"])
+            amount = row["Profit/Losses"]
+            total_amount += int(row["Profit/Losses"])
+            amount_list.append(int(row['Profit/Losses']))
+            new_budget_dict.append(
+                {
+                    "Month": row["Date"],
+                    "Amount": row["Profit/Losses"]
+                }
+            )
+        #find the monthly changes
+        for x in range(len(amount_list)):
+            changes = amount_list[x] - changes
+            avgchanges.append(changes)
+            total_changes += changes
+            changes = amount_list[x] #reset the changes variable to the previous amount
 
-# Absolute Path
-# outputpath = "/Users/aq/Documents/Classwork/vu-nsh-data-pt-02-2020-u-c/03-Python/Homework/Instructions/PyBank/Resources/pybank.txt"
-# Relative Path
-outputpath = os.path.join(os.path.expanduser('~'),"Documents/Homework/03-Python-Challenge", "Pybank","output.txt")
+    #create a new list to store the date and monthly changes    
+    changes_date_list = list(zip(month_list, avgchanges))
+    #find total number of months
+    total_months = len(new_budget_dict)
+    #find the average change over time in profit
+    avgchanges2 = round((total_changes-avgchanges[0])/(total_months-1), 2)
+    #find the max increase in profit
+    maxprofit= max(changes_date_list, key=lambda x:x[1])
+    #find the max decrease in profit
+    minprofit=min(changes_date_list, key=lambda x:x[1])
 
-#export the summary table to a text file
-with open(outputpath, "a") as f:
-    # Summary Table
-    print("Financial Analysis \n----------------------------", file =f)
-    print(f'Total Months: {total_months}', file =f)
-    print(f'Total: ${total_amount}', file =f)
-    print(f'Average Change: ${avgchanges2}', file =f)
-    print(f'Greatest Increase in Profits: {maxprofit[0]} (${maxprofit[1]})', file =f)
-    print(f'Greatest Decrease in Profits: {minprofit[0]} (${minprofit[1]})', file =f)
-    print("----------------------------", file =f)
+    # Absolute Path
+    # outputpath = "/Users/aq/Documents/Classwork/vu-nsh-data-pt-02-2020-u-c/03-Python/Homework/Instructions/PyBank/Resources/pybank.txt"
+    # Relative Path
+    outputpath = os.path.join(os.path.expanduser('~'),"Documents/Homework/Python-Challenge", "Pybank","output.txt")
 
+    #export the summary table to a text file
+    with open(outputpath, "a") as f:
+        # Summary Table
+        print("Financial Analysis \n----------------------------", file =f)
+        print(f'Total Months: {total_months}', file =f)
+        print(f'Total: ${total_amount}', file =f)
+        print(f'Average Change: ${avgchanges2}', file =f)
+        print(f'Greatest Increase in Profits: {maxprofit[0]} (${maxprofit[1]})', file =f)
+        print(f'Greatest Decrease in Profits: {minprofit[0]} (${minprofit[1]})', file =f)
+        print("----------------------------", file =f)
 
+#Run the function!
+analyze_budget(csvpath)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -10,61 +10,67 @@ import csv
 # Relative Path
 csvpath = os.path.join(os.path.expanduser('~'),"Documents/Classwork/vu-nsh-data-pt-02-2020-u-c", "03-Python/Homework/Instructions/PyPoll/Resources/election_data.csv")
 
-#create a new list to only store the candidates
-candidate_list = []
+def find_candidate(path):
 
-#open the csvfile
-with open(csvpath) as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        voteid = row["Voter ID"]
-        county = row["County"]
-        candidate_list.append(row["Candidate"])
+    #create a new list to only store the candidates
+    candidate_list = []
 
-#find the unique candidates and store that in a list
-unique_candidate_list = []
-for x in candidate_list:
-    if x not in unique_candidate_list:
-        unique_candidate_list.append(x)
+    #open the csvfile
+    with open(csvpath) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            voteid = row["Voter ID"]
+            county = row["County"]
+            candidate_list.append(row["Candidate"])
 
-for x in unique_candidate_list:
-        print(x)
+    #find the unique candidates and store that in a list
+    unique_candidate_list = []
+    for x in candidate_list:
+        if x not in unique_candidate_list:
+            unique_candidate_list.append(x)
 
-#find the total number of votes
-total_vote = len(candidate_list)
+    for x in unique_candidate_list:
+            print(x)
 
-#find the total votes for each unique candidate and store that in a list
-unique_votecount = []
-for candidate in unique_candidate_list:
-    unique_votecount.append(candidate_list.count(candidate)) 
+    #find the total number of votes
+    total_vote = len(candidate_list)
 
-#store both total votes and unique candidates in a list
-vote_candidate_list = list(zip(unique_candidate_list, unique_votecount))
-#find the candidate with max votes
-maxvote = max(vote_candidate_list, key=lambda x:x[1])
-winner = maxvote[0]
+    #find the total votes for each unique candidate and store that in a list
+    unique_votecount = []
+    for candidate in unique_candidate_list:
+        unique_votecount.append(candidate_list.count(candidate)) 
 
-#find the percentage of votes for each candidate
-c1percent = "{:.3%}".format(vote_candidate_list[0][1]/total_vote)
-c2percent = "{:.3%}".format(vote_candidate_list[1][1]/total_vote)
-c3percent = "{:.3%}".format(vote_candidate_list[2][1]/total_vote)
-c4percent = "{:.3%}".format(vote_candidate_list[3][1]/total_vote)
+    #store both total votes and unique candidates in a list
+    vote_candidate_list = list(zip(unique_candidate_list, unique_votecount))
+    #find the candidate with max votes
+    maxvote = max(vote_candidate_list, key=lambda x:x[1])
+    winner = maxvote[0]
 
-# Summary Table
-# Absolute Path
-# outputpath = "/Users/aq/Documents/Classwork/vu-nsh-data-pt-02-2020-u-c/03-Python/Homework/Instructions/PyPoll/Resources/output.txt"
-# Relative Path
-outputpath = os.path.join(os.path.expanduser('~'),"Documents/Homework/03-Python-Challenge", "PyPoll","output.txt")
+    #find the percentage of votes for each candidate
+    c1percent = "{:.3%}".format(vote_candidate_list[0][1]/total_vote)
+    c2percent = "{:.3%}".format(vote_candidate_list[1][1]/total_vote)
+    c3percent = "{:.3%}".format(vote_candidate_list[2][1]/total_vote)
+    c4percent = "{:.3%}".format(vote_candidate_list[3][1]/total_vote)
 
-#export the summary table to a text file
-with open(outputpath, "a") as f:
+    # Summary Table
+    # Absolute Path
+    # outputpath = "/Users/aq/Documents/Classwork/vu-nsh-data-pt-02-2020-u-c/03-Python/Homework/Instructions/PyPoll/Resources/output.txt"
+    # Relative Path
+    outputpath = os.path.join(os.path.expanduser('~'),"Documents/Homework/Python-Challenge", "PyPoll","output.txt")
 
-    print("Election Results \n-------------------------", file =f)
-    print(f'Total Votes: {total_vote}\n-------------------------', file =f)
-    print(f'{vote_candidate_list[0][0]}: {c1percent} ({vote_candidate_list[0][1]})', file =f)
-    print(f'{vote_candidate_list[1][0]}: {c2percent} ({vote_candidate_list[1][1]})', file =f)
-    print(f'{vote_candidate_list[2][0]}: {c3percent} ({vote_candidate_list[2][1]})', file =f)
-    print(f'{vote_candidate_list[3][0]}: {c4percent} ({vote_candidate_list[3][1]})', file =f)
-    print("-------------------------", file =f)
-    print(f'Winner: {winner}', file =f)
-    print("-------------------------", file =f)
+    #export the summary table to a text file
+    with open(outputpath, "a") as f:
+
+        print("Election Results \n-------------------------", file =f)
+        print(f'Total Votes: {total_vote}\n-------------------------', file =f)
+        print(f'{vote_candidate_list[0][0]}: {c1percent} ({vote_candidate_list[0][1]})', file =f)
+        print(f'{vote_candidate_list[1][0]}: {c2percent} ({vote_candidate_list[1][1]})', file =f)
+        print(f'{vote_candidate_list[2][0]}: {c3percent} ({vote_candidate_list[2][1]})', file =f)
+        print(f'{vote_candidate_list[3][0]}: {c4percent} ({vote_candidate_list[3][1]})', file =f)
+        print("-------------------------", file =f)
+        print(f'Winner: {winner}', file =f)
+        print("-------------------------", file =f)
+
+
+# Run the function!
+find_candidate(csvpath)
